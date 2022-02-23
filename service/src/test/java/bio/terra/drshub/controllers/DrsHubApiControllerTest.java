bfdd0152a6fcf52ea4369e72ce45781e462f168f
Kai <kfeldman@broadinstitute.org>
2022-02-23 17:29:51 -0500

    wip test 28

--- service/src/test/java/bio/terra/drshub/controllers/DrsHubApiControllerTest.java
+++ service/src/test/java/bio/terra/drshub/controllers/DrsHubApiControllerTest.java
@@ -471,6 +471,51 @@ public class DrsHubApiControllerTest extends BaseTest {
         .andExpect(status().is5xxServerError());
   }
 
+  @Test // 28
+  // todo check if it's calling the fence link, check shouldFetchFenceAccessToken
+  void testCallsBondWithFenceProviderWhenHostIsBioDataCatalyst() throws Exception {
+    var provider = "bioDataCatalyst";
+    var compactIdAndHost = getProviderHosts(provider);
+
+    var bondSaKey = Map.of("foo", "sa key");
+    mockBondLinkSaKeyApi(
+        config.getDrsProviders().get(provider).getBondProvider().get(),
+        TEST_ACCESS_TOKEN,
+        bondSaKey);
+
+    postDrsHubRequest(
+            TEST_ACCESS_TOKEN,
+            compactIdAndHost.drsUriHost,
+            UUID.randomUUID().toString(),
+            List.of(Fields.GOOGLE_SERVICE_ACCOUNT))
+        .andExpect(status().isOk())
+        .andExpect(
+            content()
+                .json(
+                    objectMapper.writeValueAsString(
+                        Map.of(
+                            Fields.GOOGLE_SERVICE_ACCOUNT,
+                            new ObjectMapper().writeValueAsString(bondSaKey)))));
+  }
+
+  //// TODO: test 28
+  // test.serial('martha_v3 calls Bond with the "fence" provider when the Data Object URL host is
+  // "dg.4503"', async (t) => {
+  //    const bond = bondUrls('fence');
+  //    const drs = drsUrls(config.HOST_BIODATA_CATALYST_STAGING);
+  //    getJsonFromApiStub.withArgs(bond.serviceAccountKeyUrl,
+  // terraAuth).resolves(googleSAKeyObject);
+  //    getJsonFromApiStub.withArgs(drs.objectsUrl, null).resolves(bdcDrsResponse);
+  //    const response = mockResponse();
+  //
+  //    await marthaV3(mockRequest({ body: { 'url': 'drs://dg.4503/this_part_can_be_anything' } }),
+  // response);
+  //
+  //    t.is(response.statusCode, 200); // Not strictly necessary here, but the test fails if we
+  // don't assert something
+  //    sinon.assert.callCount(getJsonFromApiStub, 2);
+  //  });
+
   // helper functions
 
   private ProviderHosts getProviderHosts(String provider) {
