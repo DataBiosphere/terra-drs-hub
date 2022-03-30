797f4255c0f2c957dd05634b32cd5ab19b26dba3
Sarah Wessel <swessel@broadinstitute.org>
2022-03-30 10:45:17 -0700

    [POL-181] Add missing tests (#17)
    * Remove number comments
    
    * add status and version tests
    
    * Write tests for DrsProvider
    
    * Fix spacing
    
    * use var
    
    * Use list.of
    
    * Define accessMethods list once as final

---
+++ service/src/test/java/bio/terra/drshub/config/DrsProviderTest.java
@@ -0,0 +1,79 @@
+package bio.terra.drshub.config;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import bio.terra.drshub.BaseTest;
+import bio.terra.drshub.models.AccessMethodConfigTypeEnum;
+import bio.terra.drshub.models.AccessUrlAuthEnum;
+import bio.terra.drshub.models.BondProviderEnum;
+import io.github.ga4gh.drs.model.AccessMethod.TypeEnum;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+public class DrsProviderTest extends BaseTest {
+
+  private final ProviderAccessMethodConfig testAccessMethodConfig =
+      ProviderAccessMethodConfig.create()
+          .setType(AccessMethodConfigTypeEnum.gs)
+          .setAuth(AccessUrlAuthEnum.fence_token)
+          .setFetchAccessUrl(true);
+  private final ArrayList<ProviderAccessMethodConfig> testAccessMethods =
+      new ArrayList<>(List.of(testAccessMethodConfig));
+
+  @Test
+  void testShouldFetchFenceAccessTokenReturnsTrue() {
+    // returns true when there are no problems identifying the relevant configs
+    var drsProvider = createRandomDrsProvider().setAccessMethodConfigs(testAccessMethods);
+
+    assertTrue(drsProvider.shouldFetchFenceAccessToken(TypeEnum.GS, false, false));
+  }
+
+  @Test
+  void testShouldFetchFenceAccessTokenNoBondProvider() {
+    // returns false when the access methods are valid but there's no bond provider set
+    var drsProvider =
+        createRandomDrsProvider()
+            .setAccessMethodConfigs(testAccessMethods)
+            .setBondProvider(Optional.empty());
+
+    assertFalse(drsProvider.shouldFetchFenceAccessToken(TypeEnum.GS, false, false));
+  }
+
+  @Test
+  void testShouldFetchFenceAccessTokenNoAccessMethods() {
+    // returns false when there are no access method configs
+    var drsProvider = createRandomDrsProvider().setAccessMethodConfigs(new ArrayList<>());
+
+    assertFalse(drsProvider.shouldFetchFenceAccessToken(TypeEnum.GS, false, false));
+  }
+
+  @Test
+  void testShouldFetchFenceAccessTokenNoMatchingAccessMethodTypes() {
+    // returns false when no access method configs have matching access method types
+    var drsProvider = createRandomDrsProvider().setAccessMethodConfigs(testAccessMethods);
+
+    assertFalse(drsProvider.shouldFetchFenceAccessToken(TypeEnum.FTP, false, false));
+  }
+
+  @Test
+  void testShouldFetchFenceAccessTokenNoValidAccessMethods() {
+    // returns false when there are no access method configs with fence auth method
+    var accessMethods =
+        new ArrayList<>(List.of(testAccessMethodConfig.setAuth(AccessUrlAuthEnum.passport)));
+    var drsProvider = createRandomDrsProvider().setAccessMethodConfigs(accessMethods);
+
+    assertFalse(drsProvider.shouldFetchFenceAccessToken(TypeEnum.GS, false, false));
+  }
+
+  private DrsProvider createRandomDrsProvider() {
+    return DrsProvider.create()
+        .setName(UUID.randomUUID().toString())
+        .setHostRegex(UUID.randomUUID().toString())
+        .setBondProvider(BondProviderEnum.fence)
+        .setAccessMethodConfigs(new ArrayList<>());
+  }
+}
---
+++ service/src/test/java/bio/terra/drshub/controllers/PublicApiControllerTest.java
@@ -0,0 +1,46 @@
+package bio.terra.drshub.controllers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import bio.terra.drshub.BaseTest;
+import bio.terra.drshub.config.DrsHubConfig;
+import bio.terra.drshub.config.VersionProperties;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+public class PublicApiControllerTest extends BaseTest {
+  @Autowired private MockMvc mvc;
+
+  @MockBean DrsHubConfig configMock;
+
+  @Test
+  void testGetStatus() throws Exception {
+    mvc.perform(get("/status")).andExpect(status().isOk());
+  }
+
+  @Test
+  void testGetVersion() throws Exception {
+    var versionProperties =
+        VersionProperties.create()
+            .setGitTag("gitTag")
+            .setGitHash("gitHash")
+            .setBuild("build")
+            .setGithub("github");
+    when(configMock.getVersion()).thenReturn(versionProperties);
+
+    mvc.perform(get("/version"))
+        .andExpect(status().isOk())
+        .andExpect(
+            content()
+                .json(
+                    "{\"gitTag\": \"gitTag\", \"gitHash\": \"gitHash\", "
+                        + "\"github\": \"github\", \"build\": \"build\"}"));
+  }
+}
