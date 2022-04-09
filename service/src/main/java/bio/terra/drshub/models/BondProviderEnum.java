04897d5c2bbae7cc6cae801a0bb9840905fa07d7
dvoet <dvoet@users.noreply.github.com>
2022-04-08 22:38:02 -0400

    bond providers have dashes, drs client already encodes (#26)

--- service/src/main/java/bio/terra/drshub/models/BondProviderEnum.java
+++ service/src/main/java/bio/terra/drshub/models/BondProviderEnum.java
@@ -1,8 +1,18 @@
 package bio.terra.drshub.models;
 
 public enum BondProviderEnum {
-  dcf_fence,
-  fence,
-  anvil,
-  kids_first
+  dcf_fence("dcf-fence"),
+  fence("fence"),
+  anvil("anvil"),
+  kids_first("kids-first");
+
+  private String uriValue;
+
+  BondProviderEnum(String uriValue) {
+    this.uriValue = uriValue;
+  }
+
+  public String getUriValue() {
+    return uriValue;
+  }
 }
