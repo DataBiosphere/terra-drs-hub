f042da9b7877f21a7ab1faca1eb816a5e94539ae
tlangs <tlangs@broadinstitute.org>
2022-09-26 17:27:05 -0400

    Test compactIdHosts

--- service/src/test/java/bio/terra/drshub/services/DrsProviderServiceTest.java
+++ service/src/test/java/bio/terra/drshub/services/DrsProviderServiceTest.java
@@ -11,7 +11,7 @@ class DrsProviderServiceTest extends BaseTest {
   @Autowired private DrsProviderService drsProviderService;
 
   @Test
-  void testResolvesdnsHostsAndProviders() {
+  void testResolvesDnsHostsAndProviders() {
     for (var providerName : config.getDrsProviders().keySet()) {
       var cidProviderHost = getProviderHosts(providerName);
 
@@ -26,6 +26,18 @@ class DrsProviderServiceTest extends BaseTest {
 
       var resolvedProvider = drsProviderService.determineDrsProvider(resolvedDnsUri);
       assertEquals(cidProviderHost.drsProvider(), resolvedProvider);
+    }
+  }
+
+  @Test
+  void testMapsCompactIdsToTheirFullHosts() {
+    for (var entry : config.getCompactIdHosts().entrySet()) {
+      var compactId = entry.getKey();
+      var hostName = entry.getValue();
+      var testCompactUri = String.format("drs://%s/12345", compactId);
+      var expectedUri = String.format("//%s/12345", hostName);
+      var resolvedUri = drsProviderService.getUriComponents(testCompactUri);
+      assertEquals(expectedUri, resolvedUri.toString());
     }
   }
 }
