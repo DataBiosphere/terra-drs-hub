28bc38b77e138ca58ab01f4d5142c429403c2f82
tlangs <tlangs@broadinstitute.org>
2023-05-09 09:44:13 -0400

    charsets...

--- service/src/main/java/bio/terra/drshub/services/GoogleStorageService.java
+++ service/src/main/java/bio/terra/drshub/services/GoogleStorageService.java
@@ -7,6 +7,7 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +15,8 @@ public class GoogleStorageService {
 
   public Storage getAuthedStorage(SaKeyObject saKey, String googleProject) {
     final ServiceAccountCredentials creds;
-    try (var saKeyInputStream = new ByteArrayInputStream(saKey.getData().toString().getBytes())) {
+    try (var saKeyInputStream =
+        new ByteArrayInputStream(saKey.getData().toString().getBytes(StandardCharsets.UTF_8))) {
       creds = ServiceAccountCredentials.fromStream(saKeyInputStream);
     } catch (IOException ex) {
       throw new DrsHubException("Could not parse credentials from Bond");
