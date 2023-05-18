81e053b3bf060ea653574b9f436e5fc2005577b1
Tristan Garwood <tristan.garwood@gmail.com>
2023-05-18 10:02:26 -0400

    ID-377 Fix response serialization. (#59)

--- service/src/main/java/bio/terra/drshub/models/AnnotatedResourceMetadata.java
+++ service/src/main/java/bio/terra/drshub/models/AnnotatedResourceMetadata.java
@@ -3,11 +3,13 @@ package bio.terra.drshub.models;
 import bio.terra.drshub.config.DrsProvider;
 import bio.terra.drshub.generated.model.ResourceMetadata;
 import java.util.List;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonSerialize(using = AnnotatedResourceMetadataSerializer.class)
 public class AnnotatedResourceMetadata extends ResourceMetadata {
 
   private List<String> requestedFields;
