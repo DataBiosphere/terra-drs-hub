4b491e82bcce88d9d3151c88c68f559a4aadaec7
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-12-04 17:33:16 -0500

    Move back to Spy of RestTemplateFactory in DrsApiFactoryTest
    Treat this as a sociable unit test in which RestTemplateFactory methods are invoked without mocking.
    The spying remains necessary to verify caching behavior.

--- service/src/test/java/bio/terra/drshub/services/DrsApiFactoryTest.java
+++ service/src/test/java/bio/terra/drshub/services/DrsApiFactoryTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 import bio.terra.drshub.config.DrsProvider;
 import bio.terra.drshub.config.MTlsConfig;
@@ -14,9 +13,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,7 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @ExtendWith(MockitoExtension.class)
 public class DrsApiFactoryTest {
 
-  @Mock private RestTemplateFactory restTemplateFactory;
+  @Spy private RestTemplateFactory restTemplateFactory;
   private DrsApiFactory drsApiFactory;
   private static final UriComponents URI_COMPONENTS =
       UriComponentsBuilder.newInstance().host("test").build();
@@ -41,32 +39,30 @@ public class DrsApiFactoryTest {
   @Test
   void testMTlsConfigured() {
     var mTlsConfig = MTlsConfig.create().setCertPath("fake.crt").setKeyPath("fake.key");
-    when(restTemplateFactory.makeMTlsRestTemplateWithPooling(mTlsConfig))
-        .thenReturn(new RestTemplate());
 
     drsApiFactory.getApiFromUriComponents(
         URI_COMPONENTS, createDrsProvider().setMTlsConfig(mTlsConfig));
 
+    verify(restTemplateFactory).makeMTlsRestTemplateWithPooling(any(MTlsConfig.class));
     verify(restTemplateFactory, never()).makeRestTemplateWithPooling();
   }
 
   @Test
   void testMTlsNotConfigured() {
-    when(restTemplateFactory.makeRestTemplateWithPooling()).thenReturn(new RestTemplate());
-
     drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, createDrsProvider());
 
+    verify(restTemplateFactory).makeRestTemplateWithPooling();
     verify(restTemplateFactory, never()).makeMTlsRestTemplateWithPooling(any(MTlsConfig.class));
   }
 
   @Test
   void testIndependentApiClients() {
     var drsProvider = createDrsProvider();
-    when(restTemplateFactory.makeRestTemplateWithPooling()).thenReturn(new RestTemplate());
 
     // The first ApiClient created for a DrsProvider will populate the RestTemplate cache
     var client1 = drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, drsProvider);
     verify(restTemplateFactory).makeRestTemplateWithPooling();
+    verify(restTemplateFactory, never()).makeMTlsRestTemplateWithPooling(any(MTlsConfig.class));
 
     // Subsequent ApiClients created for the same DrsProvider will reuse the cached RestTemplate
     var separateApiClient =
