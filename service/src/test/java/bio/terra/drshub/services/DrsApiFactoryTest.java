5bf0b20ff1e94f7757b9ca0e24588f8204cd7efd
Phil Shapiro <pshapiro@broadinstitute.org>
2023-12-05 15:57:49 -0500

    To avoid @Spy and to provide better test support, add a layer between ApiClient and its arguments to we can see when it's called and with what arguments.

--- service/src/test/java/bio/terra/drshub/services/DrsApiFactoryTest.java
+++ service/src/test/java/bio/terra/drshub/services/DrsApiFactoryTest.java
@@ -1,20 +1,21 @@
 package bio.terra.drshub.services;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import bio.terra.drshub.config.DrsProvider;
 import bio.terra.drshub.config.MTlsConfig;
+import io.github.ga4gh.drs.client.ApiClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Spy;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -22,14 +23,20 @@ import org.springframework.web.util.UriComponentsBuilder;
 @ExtendWith(MockitoExtension.class)
 class DrsApiFactoryTest {
 
-  @Spy private RestTemplateFactory restTemplateFactory;
+  @Mock private RestTemplateFactory restTemplateFactory;
+  @Mock private DrsApiClientFactory drsApiClientFactory;
+  @Mock private ApiClient apiClient;
+  @Mock private RestTemplate restTemplate;
   private DrsApiFactory drsApiFactory;
   private static final UriComponents URI_COMPONENTS =
       UriComponentsBuilder.newInstance().host("test").build();
 
   @BeforeEach
   void setup() {
-    drsApiFactory = new DrsApiFactory(restTemplateFactory);
+    drsApiFactory = new DrsApiFactory(restTemplateFactory, drsApiClientFactory);
+
+    when(drsApiClientFactory.createClient(restTemplate)).thenReturn(apiClient);
+    when(apiClient.getBasePath()).thenReturn("");
   }
 
   private DrsProvider createDrsProvider() {
@@ -38,21 +45,25 @@ class DrsApiFactoryTest {
 
   @Test
   void testMTlsConfigured() {
-    var mTlsConfig = MTlsConfig.create().setCertPath("fake.crt").setKeyPath("fake.key");
+    var mTlsConfig = MTlsConfig.create();
+    when(restTemplateFactory.makeMTlsRestTemplateWithPooling(mTlsConfig)).thenReturn(restTemplate);
 
-    drsApiFactory.getApiFromUriComponents(
-        URI_COMPONENTS, createDrsProvider().setMTlsConfig(mTlsConfig));
+    var drsApi =
+        drsApiFactory.getApiFromUriComponents(
+            URI_COMPONENTS, createDrsProvider().setMTlsConfig(mTlsConfig));
+    assertThat(drsApi.getApiClient(), is(apiClient));
 
-    verify(restTemplateFactory).makeMTlsRestTemplateWithPooling(any(MTlsConfig.class));
-    verify(restTemplateFactory, never()).makeRestTemplateWithPooling();
+    verify(drsApiClientFactory).createClient(restTemplate);
   }
 
   @Test
   void testMTlsNotConfigured() {
-    drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, createDrsProvider());
+    when(restTemplateFactory.makeRestTemplateWithPooling()).thenReturn(restTemplate);
 
-    verify(restTemplateFactory).makeRestTemplateWithPooling();
-    verify(restTemplateFactory, never()).makeMTlsRestTemplateWithPooling(any(MTlsConfig.class));
+    var drsApi = drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, createDrsProvider());
+    assertThat(drsApi.getApiClient(), is(apiClient));
+
+    verify(drsApiClientFactory).createClient(restTemplate);
   }
 
   @Test
@@ -60,21 +71,18 @@ class DrsApiFactoryTest {
     var drsProvider = createDrsProvider();
 
     // The first ApiClient created for a DrsProvider will populate the RestTemplate cache
-    var client1 = drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, drsProvider);
+    when(restTemplateFactory.makeRestTemplateWithPooling()).thenReturn(restTemplate);
+    drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, drsProvider);
     verify(restTemplateFactory).makeRestTemplateWithPooling();
-    verify(restTemplateFactory, never()).makeMTlsRestTemplateWithPooling(any(MTlsConfig.class));
 
     // Subsequent ApiClients created for the same DrsProvider will reuse the cached RestTemplate
-    var separateApiClient =
-        spy(drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, drsProvider).getApiClient());
-    verifyNoMoreInteractions(restTemplateFactory);
+    drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, drsProvider);
+    // If there was a cache miss, this would fail as the API would be called twice.
+    verify(restTemplateFactory).makeRestTemplateWithPooling();
 
-    // Setting the token or headers on an ApiClient will have no effect on ApiClients created for
-    // the same DrsProvider
-    client1.setBearerToken("bearer-token");
-    verifyNoInteractions(separateApiClient);
-
-    client1.setHeader("test-header", "test-value");
-    verifyNoInteractions(separateApiClient);
+    // Force a cache miss by using a different provider name. Now we should see two calls to the rest template factory.
+    drsProvider.setName("another name");
+    drsApiFactory.getApiFromUriComponents(URI_COMPONENTS, drsProvider);
+    verify(restTemplateFactory, times(2)).makeRestTemplateWithPooling();
   }
 }
