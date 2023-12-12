42d054012531ce137e673b6ddac7347578159b9d
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-12-11 19:10:07 -0500

    Migrate Apache HttpClient 4->5
    Using https://hc.apache.org/httpcomponents-client-5.3.x/migration-guide/preparation.html as a helper.
    Most old interfaces / classes resolved automatically after swapping org.apache.http imports with org.apache.hc.httpclient5, but SSL settings required extra attention.
    We now set the SSL socket factory on the PoolingConnectionManager which is passed to our CloseableHttpClient.

--- service/src/main/java/bio/terra/drshub/services/RestTemplateFactory.java
+++ service/src/main/java/bio/terra/drshub/services/RestTemplateFactory.java
@@ -3,14 +3,13 @@ package bio.terra.drshub.services;
 import bio.terra.drshub.config.DrsHubConfig;
 import bio.terra.drshub.config.MTlsConfig;
 import nl.altindag.ssl.SSLFactory;
-import nl.altindag.ssl.util.Apache4SslUtils;
-import nl.altindag.ssl.util.PemUtils;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import nl.altindag.ssl.apache5.util.Apache5SslUtils;
+import nl.altindag.ssl.pem.util.PemUtils;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.socket.LayeredConnectionSocketFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -26,15 +25,7 @@ public class RestTemplateFactory {
 
   /** @return a new RestTemplate backed by a pooling connection manager */
   public RestTemplate makeRestTemplateWithPooling() {
-    PoolingHttpClientConnectionManager poolingConnManager =
-        new PoolingHttpClientConnectionManager();
-    poolingConnManager.setMaxTotal(connectionPoolSize);
-    poolingConnManager.setDefaultMaxPerRoute(connectionPoolSize);
-    CloseableHttpClient httpClient =
-        HttpClients.custom().setConnectionManager(poolingConnManager).build();
-    HttpComponentsClientHttpRequestFactory factory =
-        new HttpComponentsClientHttpRequestFactory(httpClient);
-    return new RestTemplate(factory);
+    return makeRestTemplateWithPooling(null);
   }
 
   /**
@@ -45,19 +36,26 @@ public class RestTemplateFactory {
     var keyManager =
         PemUtils.loadIdentityMaterial(mTlsConfig.getCertPath(), mTlsConfig.getKeyPath());
     var sslFactory = SSLFactory.builder().withIdentityMaterial(keyManager).build();
-    var socketFactory = Apache4SslUtils.toSocketFactory(sslFactory);
-    Registry<ConnectionSocketFactory> socketFactoryRegistry =
-        RegistryBuilder.<ConnectionSocketFactory>create().register("https", socketFactory).build();
+    var socketFactory = Apache5SslUtils.toSocketFactory(sslFactory);
 
-    PoolingHttpClientConnectionManager poolingConnManager =
-        new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-    poolingConnManager.setMaxTotal(connectionPoolSize);
-    poolingConnManager.setDefaultMaxPerRoute(connectionPoolSize);
+    return makeRestTemplateWithPooling(socketFactory);
+  }
+
+  /**
+   * @return a new RestTemplate backed by a pooling connection manager with its SSL socket factory
+   *     set (if specified)
+   */
+  private RestTemplate makeRestTemplateWithPooling(LayeredConnectionSocketFactory socketFactory) {
+    var poolingConnManagerBuilder =
+        PoolingHttpClientConnectionManagerBuilder.create()
+            .setMaxConnTotal(connectionPoolSize)
+            .setMaxConnPerRoute(connectionPoolSize);
+    if (socketFactory != null) {
+      poolingConnManagerBuilder.setSSLSocketFactory(socketFactory);
+    }
+    PoolingHttpClientConnectionManager poolingConnManager = poolingConnManagerBuilder.build();
     CloseableHttpClient httpClient =
-        HttpClients.custom()
-            .setSSLSocketFactory(socketFactory)
-            .setConnectionManager(poolingConnManager)
-            .build();
+        HttpClients.custom().setConnectionManager(poolingConnManager).build();
     HttpComponentsClientHttpRequestFactory factory =
         new HttpComponentsClientHttpRequestFactory(httpClient);
     return new RestTemplate(factory);
