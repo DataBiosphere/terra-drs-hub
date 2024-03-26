306a04a0971102c536a36c7751091cd666ce2b74
Saman Ehsan <sehsan@broadinstitute.org>
2024-03-26 12:40:24 -0400

    Use default RestTemplate in ECM API factory
    ECM does not return only JSON responses now,
    so the jackson converter should not be set as
    the only HTTP message converter. The default
    rest template includes the jackson converter
    already.

--- service/src/main/java/bio/terra/drshub/services/ExternalCredsApiFactory.java
+++ service/src/main/java/bio/terra/drshub/services/ExternalCredsApiFactory.java
@@ -5,23 +5,14 @@ import bio.terra.externalcreds.api.FenceAccountKeyApi;
 import bio.terra.externalcreds.api.OauthApi;
 import bio.terra.externalcreds.api.OidcApi;
 import bio.terra.externalcreds.client.ApiClient;
-import java.util.List;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public record ExternalCredsApiFactory(
-    DrsHubConfig drsHubConfig, MappingJackson2HttpMessageConverter jacksonConverter) {
+public record ExternalCredsApiFactory(DrsHubConfig drsHubConfig) {
 
   public ApiClient getApi(String accessToken) {
-    // ECM returns lots of responses as JSON string.
-    // By default, these get intercepted by Spring's raw string converter, but they're quoted, so
-    // they should be parsed as JSON.
-    // N.B. This means that any non-JSON responses won't be parsed correctly, so if ECM ever returns
-    // any other content-type, this would have to be revised. However, that seems unlikely.
-    var restTemplate = new RestTemplate(List.of(jacksonConverter));
-
+    var restTemplate = new RestTemplate();
     var client = new ApiClient(restTemplate);
     client.setBasePath(drsHubConfig.getExternalcredsUrl());
     client.setAccessToken(accessToken);
