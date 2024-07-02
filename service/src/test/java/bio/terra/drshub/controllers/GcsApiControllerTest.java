709de9ce4dfceebe5bff8cb13a7ed833d1d198fd
Kai <kfeldman@broadinstitute.org>
2024-07-02 17:02:31 -0400

    cleanup

--- service/src/test/java/bio/terra/drshub/controllers/GcsApiControllerTest.java
+++ service/src/test/java/bio/terra/drshub/controllers/GcsApiControllerTest.java
@@ -43,7 +43,6 @@ public class GcsApiControllerTest extends BaseTest {
     var drsUri = "drs://dg.4503:1234/456/2315asd";
     var bucketName = "my-test-bucket";
     var objectName = "my-test-folder/my-test-object.txt";
-    var ip = "test.ip";
     var googleProject = "test-google-project";
     var url = new URL("https", "storage.cloud.google.com", "/" + bucketName + "/" + objectName);
 
@@ -59,7 +58,6 @@ public class GcsApiControllerTest extends BaseTest {
     var drsUri = "drs://dg.4503:1234/456/2315asd";
     var bucketName = "my-test-bucket";
     var objectName = "my-test-folder/my-test-object.txt";
-    var ip = "test.ip";
     var googleProject = "test-google-project";
     var url = new URL("https", "storage.cloud.google.com", "/" + bucketName + "/" + objectName);
 
