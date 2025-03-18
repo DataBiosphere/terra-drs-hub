16f9ecec0bb89e9c13407e051aab519a1c7a7ca4
Phil Shapiro <pshapiro@broadinstitute.org>
2025-03-18 13:10:40 -0400

    update render_config.sh; fix otel compile issue

--- render_config.sh
+++ render_config.sh
@@ -1,26 +1,22 @@
 ENV=${1:-dev}
-VAULT_TOKEN=${2:-$(cat "$HOME"/.vault-token)}
 
-VAULT_ADDR="https://clotho.broadinstitute.org:8200"
-DRSHUB_VAULT_PATH="secret/dsde/terra/kernel/$ENV/$ENV/drshub"
-COMMON_VAULT_PATH="secret/dsde/terra/kernel/$ENV/common"
-
-VAULT_COMMAND="vault read"
+if [[ $ENV == dev ]]; then
+    GCLOUD_PROJECT=broad-dsde-dev
+elif [[ $ENV == integration ]]; then
+    GCLOUD_PROJECT=broad-dsde-qa
+else
+    echo "Invalid environment: $ENV"
+    exit 1
+fi
 
 SERVICE_OUTPUT_LOCATION="$(dirname "$0")/service/src/main/resources/rendered"
 INTEGRATION_OUTPUT_LOCATION="$(dirname "$0")/integration/src/main/resources/rendered"
 
-if ! [ -x "$(command -v vault)" ]; then
-  VAULT_COMMAND="docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=$VAULT_ADDR vault:1.7.3 $VAULT_COMMAND"
-fi
+GET_SECRET="gcloud secrets versions access latest --project $GCLOUD_PROJECT --secret"
 
-$VAULT_COMMAND -field=value "$DRSHUB_VAULT_PATH/ras-mtls-client.crt" >"$SERVICE_OUTPUT_LOCATION/ras-mtls-client.crt"
-$VAULT_COMMAND -field=value "$DRSHUB_VAULT_PATH/ras-mtls-client.key" >"$SERVICE_OUTPUT_LOCATION/ras-mtls-client.key"
-$VAULT_COMMAND -field=swagger-client-id "$DRSHUB_VAULT_PATH/swagger-client-id" >"$SERVICE_OUTPUT_LOCATION/swagger-client-id"
+$GET_SECRET drshub-ras-mtls-client-cert >"$SERVICE_OUTPUT_LOCATION/ras-mtls-client.crt"
+$GET_SECRET drshub-ras-mtls-client-key >"$SERVICE_OUTPUT_LOCATION/ras-mtls-client.key"
+$GET_SECRET drshub-swagger-client-id >"$SERVICE_OUTPUT_LOCATION/swagger-client-id"
 
 
-$VAULT_COMMAND -field=data -format=json "secret/dsde/firecloud/$ENV/common/firecloud-account.json" >"$INTEGRATION_OUTPUT_LOCATION/user-delegated-sa.json"
-
-if [ $ENV == perf ]; then
-  $VAULT_COMMAND -field=key "$COMMON_VAULT_PATH/testrunner/testrunner-sa" | base64 -d > "$INTEGRATION_OUTPUT_LOCATION/testrunner-sa.json"
-fi
+$GET_SECRET firecloud-sa >"$INTEGRATION_OUTPUT_LOCATION/user-delegated-sa.json"
