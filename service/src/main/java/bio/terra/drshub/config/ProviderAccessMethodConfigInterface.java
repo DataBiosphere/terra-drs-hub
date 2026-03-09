0a68f8f60c1015182479e74affd69a70534e373e
Kevin Marete <kevinmmarete@gmail.com>
2026-03-09 15:23:42 -0400

    Add retry logic

--- service/src/main/java/bio/terra/drshub/config/ProviderAccessMethodConfigInterface.java
+++ service/src/main/java/bio/terra/drshub/config/ProviderAccessMethodConfigInterface.java
@@ -4,6 +4,7 @@ import bio.terra.drshub.models.AccessMethodConfigTypeEnum;
 import bio.terra.drshub.models.DrsAuthEnum;
 import java.util.Optional;
 import org.immutables.value.Value;
+import org.immutables.value.Value.Default;
 
 @Value.Modifiable
 @PropertiesInterfaceStyle
@@ -15,4 +16,15 @@ public interface ProviderAccessMethodConfigInterface {
   boolean isFetchAccessUrl();
 
   Optional<DrsAuthEnum> getFallbackAuth();
+
+  /**
+   * When true, DrsHub first attempts the access-URL call WITHOUT forwarding x-user-project. If the
+   * provider returns 400 with an indication that the user project is required, DrsHub retries with
+   * the header. This allows TDR snapshots that set requireUserProject=true to direct DrsHub to bill
+   * the caller's project, without requiring callers to special-case TDR.
+   */
+  @Default
+  default boolean requiresUserProjectOnRetry() {
+    return false;
+  }
 }
